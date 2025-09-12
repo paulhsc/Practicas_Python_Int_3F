898e6997fad4dfeacaa948556cafe2ca89c084e0
paulhsc <paulhsc@gmail.com>
2025-09-12 13:38:01 -0300

    Update Practica_2.Excepciones.py

--- Practica_2.Excepciones.py
+++ Practica_2.Excepciones.py
@@ -11,8 +11,9 @@ print('\n==== 2.')
 
 print('\n==== 3.')
 #print(d['edad'])
-'''
+
 print('\n==== 4.')
+'''
 with open(n_ar) as archivo:
   datos = archivo.read()
   print(f'Los datos del archivo con: {datos}')
@@ -21,4 +22,5 @@ print('\n==== 5.')
 
 n1 = int(input('Ingresa 1er num (decimal para ValueError: '))
 n2 = int(input('Ingresa 2do num(0 para ZeroDivisionError: '))
+
 print(n1/n2)
