0b1056ec5253936c791efa2b6bb7bfdce260c914
paulhsc <paulhsc@gmail.com>
2025-09-12 17:04:45 -0300

    Delete Práctica 2 directory

--- Práctica 2/3.py
+++
@@ -1,6 +0,0 @@
-#Escribe un programa que intente acceder a una clave que no existe en un diccionario.
-# Si se produce una excepción KeyError, captura la excepción y muestra
-
-d = {'nombre': 'Juan','sexo': 'M'}
-print('\n==== 3. KeyError')
-print(d['edad'])
--- Práctica 2/5.py
+++
@@ -1,9 +0,0 @@
-# Escribe un programa que intente dividir dos números. Si el segundo número es cero, captura la
-# excepción ZeroDivisionError. Si el primer número es un número no válido, captura la excepción
-# ValueError. En cualquier caso, muestra un mensaje de error al usuario.
-
-print('\n==== 5. ZeroDivisionError / ValueError')
-
-n1 = int(input('Ingresa 1er num (decimal para ValueError: '))
-n2 = int(input('Ingresa 2do num(0 para ZeroDivisionError: '))
-print(n1/n2)
